48f7503c939887d86e76a2a6beeca1bddfb528e4
Mateusz <gorzanski.mateusz@gmail.com>
2018-08-13 14:45:23 +0200

    set android.enableAapt2 to false and add props validation to Icon component

--- app/utils/Icon.js
+++ app/utils/Icon.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import { StyleProvider, getTheme, Icon as IconBase } from 'native-base';
+import React, { Component } from "react";
+import { StyleProvider, getTheme, Icon as IconBase } from "native-base";
+import PropTypes from "prop-types";
 
 export default class Icon extends Component {
-  render () {
+  render() {
     const { family, name, style } = this.props;
-    const icon = <IconBase name={name} style={style} />
+    const icon = <IconBase name={name} style={style} />;
     if (family) {
-      const customTheme = getTheme({iconFamily: family})
-      return <StyleProvider style={customTheme}>{icon}</StyleProvider>
+      const customTheme = getTheme({ iconFamily: family });
+      return <StyleProvider style={customTheme}>{icon}</StyleProvider>;
     } else {
-      return icon
+      return icon;
     }
   }
 }
+
+Icon.propTypes = {
+  family: PropTypes.string,
+  name: PropTypes.string,
+  style: PropTypes.object
+};
